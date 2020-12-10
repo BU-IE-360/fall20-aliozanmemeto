e311a3ac28e6dafcc615edbebdc017d7ad920888
aliozanmemeto <69462206+aliozanmemeto@users.noreply.github.com>
2020-12-10 22:45:29 +0300

    HW2 - first commit

---
+++ files/HW2.R
@@ -0,0 +1,103 @@
+library('data.table', quietly = TRUE)
+library('ggplot2', quietly = TRUE)
+library('dplyr')
+library('lubridate', quietly = TRUE)
+library('readxl')
+library('zoo')
+library ('fpp2')
+library('ggcorrplot')
+number_of_employed <- data.table(read_xlsx('/Users/aliozan.memetoglu/Downloads/İstihdam edilenler.xlsx'))[,c('Tarih', 'TP TIG03')]
+number_of_exported <- data.table(read_xlsx("/Users/aliozan.memetoglu/Downloads/Genel ticaret sistemine göre dış ticaret ekonomik faaliyetlere göre ihracat.xlsx"))[,c('Tarih','TP IHRISICREV4 TT')]
+industrial_production_index <- data.table(read_xlsx('/Users/aliozan.memetoglu/Downloads/Sanayi Üretim Endeksi.xlsx' ))[,c('Tarih', 'TP SANAYREV4 Y1')]
+
+
+colname_filterdate <- function(someDT,old_colname,new_colname){
+  
+  setnames(someDT, 'Tarih', 'date')
+  someDT <- someDT %>% filter(grepl('-', date))
+  
+  setnames(someDT, old_colname, new_colname)
+  someDT[,date := as.Date(as.yearmon(date))]
+  someDT <- someDT[date >=  ymd(20130101) & date <  ymd(20200101) ]
+  
+  someDT$month <- format( someDT$date, "%m")
+  someDT$year <- format( someDT$date,"%Y")
+  someDT$day <- as.numeric( format(someDT$date, "%d"))
+  # because there is no forecasting in this study, no further time tag extraction is applied
+  
+  someDT[,data:=as.numeric(data)]
+  
+  return(someDT)
+}
+
+
+number_of_employed <- colname_filterdate(number_of_employed, 'TP TIG03', 'data')
+number_of_exported <- colname_filterdate(number_of_exported, 'TP IHRISICREV4 TT', 'data')
+industrial_production_index <- colname_filterdate(industrial_production_index, 'TP SANAYREV4 Y1', 'data')
+
+
+ggplot(industrial_production_index, aes(x=date, y=data)) +
+  geom_line()+ geom_smooth(method = "lm", color = 'lightblue')+
+  ylim(50, 150)+
+  labs(title = "Monthly Industrial Production Index over 2013-2019", 
+       x = "IPI",
+       y = "Month of Year") 
+
+
+ggplot(number_of_employed, aes(x=date, y=data)) + geom_line()+
+  geom_smooth(method = "lm", color = 'lightgreen')+
+  ylim(23000, 30000)+
+  labs(title = "Monthly Number of Employed People over 2013-2019", 
+       x = "Number of Employed People",
+       y = "Month of Year") 
+
+  
+ggplot(number_of_exported, aes(x=date, y=data)) + 
+  geom_line() +
+  geom_smooth(method = "lm", color = 'lightpink') +
+  ylim(9900000, 17000000)+
+  labs(title = "Monthly Number of Exported Products over 2013-2019", 
+       x = "Number of Exported Products",
+       y = "Month of Year") 
+
+#+facet_wrap( ~ year, scales = "free_x")
+
+number_of_employed_seasonal <- ts(number_of_employed[,"data"],frequency=12, start=c(2013,1))
+industrial_production_index_seasonal <- ts(industrial_production_index[,"data"],frequency=12, start=c(2013,1))
+number_of_exported_seasonal <- ts(number_of_exported[,"data"],frequency=12, start=c(2013,1))
+
+
+autoplot(Acf(number_of_exported_seasonal,plot=FALSE))+
+  ggtitle("Autocorrelation Function of Fed Funds Rate")
+
+
+
+
+
+
+ggseasonplot(number_of_employed_seasonal, year.labels=TRUE, year.labels.left=TRUE) +
+  theme(plot.title = element_text(hjust = 0.5)) +  # to center the plot title
+  ylab("Number of Employed People by Year") +
+  ggtitle("Monthly Number of Employed People over 2013-2019")
+
+
+ggplot(number_of_employed, aes(x=data)) +
+  geom_histogram(aes(y=..density..), colour="lightgreen",fill='white', bins = 50)+ 
+  geom_density(alpha=.4, fill="#FF6666") +
+  labs(title = "Histograms of the Total Number of Covid Cases in Every Month", 
+       x = "Covid Cases",
+       y = "Frequency")  +
+  scale_y_continuous()+
+  
+  theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+  facet_wrap(~year, ncol = 3)
+
+
+total_dataset = cbind(employed = number_of_employed$data, ipi = industrial_production_index$data,exported =number_of_exported$data)
+ggcorrplot(cor(total_dataset), hc.order = TRUE,
+           lab = TRUE)
+
+
+print(cor.test(total_dataset[,'employed'], total_dataset[,'ipi']))
+
+
--- index.md
+++ index.md
@@ -5,3 +5,5 @@
 1 - [Here](files/homework_0.html) is my 5 interesting examples.
 
 2 - [Here](files/HW1.html) is my HW1 "Data Visualization with Google Trends".
+
+3- [Here](files/HW2.html) is my HW2 "A Time Series Analysis of Different Economy Related Metrics in Turkey".
